4380c5e5b6f6404edde284fd7e095534b01b8046
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-04-07 20:59:42 +0500

    CGPA DOne

--- src/components/CGPA_Calculator.jsx
+++ src/components/CGPA_Calculator.jsx
@@ -3,24 +3,48 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-
 export default function CGPA() {
+    const [Input, setInput] = React.useState({
+        SGPA0: "",SGPA1:"",SGPA2:"",SGPA3:"",SGPA4:"",SGPA5:"",SGPA6:"",SGPA7:""
+    });
     const SemesterNum = [1];
-    const handleClick = ()=>{
 
-    }
+    function handleClick(e){
+        // const {SGPA0,SGPA1,SGPA2,SGPA3,SGPA4,SGPA5,SGPA6,SGPA7}=Input;
+        let sum=0,counter=0;
+        for(let i in Input){
+            if(Input[i]){
+                sum=sum+parseFloat(Input[i])
+                counter+=1
+            }
+        }
+        console.log(sum/counter);
+        e.preventDefault();
+    };
     const [addSemester, setaddSemester] = React.useState(SemesterNum);
     function AddSemester() {
-        setaddSemester([...addSemester,addSemester[addSemester.length-1]+1])
+        setaddSemester([
+            ...addSemester,
+            addSemester[addSemester.length - 1] + 1,
+        ]);
     }
     function clearAll() {
-        setaddSemester([addSemester])
+        setaddSemester([addSemester]);
+        setInput({SGPA0: "",SGPA1:"",SGPA2:"",SGPA3:"",SGPA4:"",SGPA5:"",SGPA6:"",SGPA7:""})
     }
     return (
         <form className="GPA-container">
             <h3>Cumulative GPA</h3>
-            {addSemester.map((e,id) => {
-                return (<InputCGPA key={id}/>)
+            {addSemester.map((e, id) => {
+                return (
+                    <InputCGPA
+                        key={id}
+                        index={id}
+                        name={`SGPA${id}`}
+                        value={Input[`SGPA${id}`]}
+                        change={setInput}
+                    />
+                );
             })}
             <div className="icons">
                 <div className="add" onClick={AddSemester}>
@@ -38,7 +62,13 @@ export default function CGPA() {
                     Clear All
                 </div>
             </div>
-            <button type="submit" className="calculate-gpa" onClick={handleClick}>Calculate</button>
+            <button
+                type="submit"
+                className="calculate-gpa"
+                onClick={handleClick}
+            >
+                Calculate
+            </button>
         </form>
     );
 }
--- src/components/inputFields.jsx
+++ src/components/inputFields.jsx
@@ -1,7 +1,11 @@
 function InputGPA() {
     return (
         <div className="inputs">
-            <input className="subjectName" type="text" placeholder="Subject Name" />
+            <input
+                className="subjectName"
+                type="text"
+                placeholder="Subject Name"
+            />
             <select className="options" name="Grade" id="">
                 <option value="A">A</option>
                 <option value="B+">B+</option>
@@ -16,13 +20,29 @@ function InputGPA() {
         </div>
     );
 }
-function InputCGPA(){
+function InputCGPA(props) {
+    function handleChange(e){
+        return props.change((preValue) => {
+            return { ...preValue, [e.target.name]: e.target.value };
+        });
+    }
     return (
         <div className="cgpa-inputs">
-            <input className="subjectName" type="text" placeholder="Semester No" />
-            <input className="credits" type="text" placeholder="SGPA" />
+            <input
+                className="subjectName"
+                type="text"
+                placeholder="Semester No"
+            />
+            <input
+                className="credits"
+                name={props.name}
+                type="text"
+                placeholder="SGPA"
+                value={props.value}
+                onChange={handleChange}
+            />
         </div>
     );
 }
 
-export {InputCGPA,InputGPA}
+export { InputCGPA, InputGPA };
