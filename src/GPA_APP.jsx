527788e3df543751a58f0b2f582efd0756327da3
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-04-08 02:22:37 +0500

    CGPA Done

--- src/GPA_APP.jsx
+++ src/GPA_APP.jsx
@@ -2,13 +2,17 @@ import GPA from "./components/GPA_Calculator";
 import React from "react";
 
 export default function GPA_App(props) {
+    const [result,setResult] = React.useState("");
     return (
         <>
             <div className="title">GPA Calculator</div>
-            <GPA />
+            <GPA set={setResult}/>
             <div className="change-div">
                 For Cumulative GPA?{" "}
                 <span onClick={() => props.onSwitch("CGPA")}>Click Here</span>
+            </div>
+            <div className="result" data-result>
+                Your CGPA is: {result}
             </div>
         </>
     );
